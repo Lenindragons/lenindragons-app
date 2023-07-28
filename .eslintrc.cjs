ca0fd82496ca7cec96763889eb11f6eac460087d
Ju Dantas <jucienyds@gmail.com>
2023-07-27 22:09:11 -0300

    feat: add react router dom v6

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,5 +33,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/react-in-jsx-scope': 'off',
     'import/no-extraneous-dependencies': 'off',
+    'react/function-component-definition': 'off',
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
-export function App() {
+export const App = () => {
   return <h1>Hello World</h1>
 }
