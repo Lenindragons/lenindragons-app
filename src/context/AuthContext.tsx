9e8f50e0c58b9dd0a93996a298584da85759a4b6
Ju Dantas <jucienyds@gmail.com>
2023-08-20 18:46:47 -0300

    feat: create session with firebase

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,30 +1,61 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
-import { ReactNode, createContext, useContext, useState } from 'react'
+import {
+  GoogleAuthProvider,
+  UserInfo,
+  getAuth,
+  signInWithPopup,
+} from 'firebase/auth'
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 import { app } from '../services/firebaseConfig'
 
 const provider = new GoogleAuthProvider()
 
 const AuthContext = createContext({})
 
-interface Props {
+type Props = {
   children: ReactNode
 }
 
+type User = {
+  name: string | null
+  email: string | null
+  image: string | null
+}
+
 const AuthProvider = ({ children }: Props) => {
-  const [user, setUser] = useState<any>(null)
+  const [user, setUser] = useState<User>({ name: '', email: '', image: '' })
   const [, setCredential] = useState<any>(null)
 
   const auth = getAuth(app)
+
+  const getUserInfo = (userInfo: UserInfo) => {
+    return (
+      userInfo && {
+        name: userInfo.displayName,
+        email: userInfo.email,
+        image: userInfo.photoURL,
+      }
+    )
+  }
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userInfo) => {
+      if (userInfo) {
+        setUser(getUserInfo(userInfo))
+      }
+    })
+  }, [auth])
 
   const signInGoogle = () => {
     signInWithPopup(auth, provider)
       .then((res) => {
         setCredential(GoogleAuthProvider.credentialFromResult(res))
-        setUser({
-          name: res.user.displayName,
-          email: res.user.email,
-          image: res.user.photoURL,
-        })
+        setUser(getUserInfo(res.user))
       })
       .catch((err) => {
         console.log(GoogleAuthProvider.credentialFromError(err))
