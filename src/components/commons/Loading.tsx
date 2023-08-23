c1371e2c04a8127b5aa4e33ed917d5442a7c9c5d
Ju Dantas <jucienyds@gmail.com>
2023-08-22 23:58:02 -0300

    feat: create loading component

---
+++ src/components/commons/Loading.tsx
@@ -0,0 +1,19 @@
+import ReactLoading from 'react-loading'
+import { styled, useTheme } from 'styled-components'
+
+const LoadingContainer = styled.div`
+  display: grid;
+  place-items: center;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+`
+
+export const Loading = () => {
+  const theme = useTheme()
+  return (
+    <LoadingContainer>
+      <ReactLoading type="spin" color={theme.colors.primary} width={80} />
+    </LoadingContainer>
+  )
+}
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,11 +1,12 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
+import { Loading } from '../components/commons/Loading'
 
 const PrivateRoute = () => {
   const { loading, user } = useAuth()
 
   if (loading) {
-    return <>Carregando!</>
+    return <Loading />
   }
 
   return user ? <Outlet /> : <Navigate to="/" />
