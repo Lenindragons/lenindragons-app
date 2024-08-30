c27c932233a613a8c5834cb988a1255fb645a032
Ju Dantas <jucieny.santos@grupoboticario.com.br>
2024-08-30 16:04:16 -0300

    feat: added firestore cache

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,4 +1,4 @@
-import { getFirestore } from '@firebase/firestore'
+import { memoryLocalCache, initializeFirestore } from '@firebase/firestore'
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
@@ -12,4 +12,5 @@ const firebaseConfig = {
 }
 
 export const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
+
+export const db = initializeFirestore(app, { localCache: memoryLocalCache() })
