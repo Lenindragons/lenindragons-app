a466a262a6499d658d0fbf82bfaaf793f9f5c429
Ju Dantas <jucienyds@gmail.com>
2025-05-04 09:04:02 -0300

    feat: added alert in achievements page

--- src/pages/achievements/AchievementsDetailPage.tsx
+++ src/pages/achievements/AchievementsDetailPage.tsx
@@ -3,7 +3,7 @@ import { getDate } from "@/helpers/format-date"
 import { getAchievements } from "@/services/achievements"
 import { WebPageTemplate } from "@/templates/webpage/WebPage"
 import { CatchingPokemon } from "@mui/icons-material"
-import { Alert, Box, Chip, Paper, Typography } from "@mui/material"
+import { Alert, AlertTitle, Box, Chip, Paper, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 
 export const AchievementsDetailPage = () => {
@@ -27,6 +27,12 @@ export const AchievementsDetailPage = () => {
   return (
     <WebPageTemplate>
       <Typography variant="h4" m={3} ><strong>Atividades de liga</strong> disponiveis</Typography>
+
+      <Alert severity="warning" style={{ width: "100%", marginBottom: "15px", border: "1px solid orange" }}>
+        <AlertTitle><strong>Atenção</strong></AlertTitle>
+        Procure uma organizadora para creditar seus pontos de atividade. É necessário um comprovante de participação.
+        <p><strong>(foto com data e nome na lista do torneio)</strong></p>
+      </Alert>
 
       <Box component={Paper} p={2}>
         {achievements.map((achievement: any) => (
